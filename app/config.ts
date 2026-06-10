29a9c4f14fd3086c30b2d052c796805fb043171e
SP007ZA <maphanga1sp@gmail.com>
2026-06-10 18:44:54 +0200

    update_URL

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4000/api/graphql`;
-export const prodEndpoint = `https://vertical-belkin-ridge-click.trycloudflare.com/api/graphql`;
+export const prodEndpoint = `https://wines-roger-batch-miles.trycloudflare.com/api/graphql`;
 export const wsEndpoint = `ws://localhost:4000/api/graphql`
-export const wsProdEndpoint = `wss://vertical-belkin-ridge-click.trycloudflare.com/api/graphql`
+export const wsProdEndpoint = `wss://wines-roger-batch-miles.trycloudflare.com/api/graphql`
 
